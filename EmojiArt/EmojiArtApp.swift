a173d8708c9187fa0959e9a446876060d1526df2
fllnrn <lazy@home.local>
2021-10-06 16:08:15 +0300

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Андрей Гавриков on 06.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EmojiArt/EmojiArtModel.Background.swift
@@ -0,0 +1,33 @@
+//
+//  EmojiArtModel.Background.swift
+//  EmojiArt
+//
+//  Created by Андрей Гавриков on 06.10.2021.
+//
+
+import Foundation
+
+
+extension EmojiArtModel {
+    enum Background {
+        case blank
+        case url(URL)
+        case imageData(Data)
+        
+        var url: URL? {
+            switch self {
+            case .url(let url): return url
+            default: return nil
+            }
+        }
+        
+        var imageData: Data? {
+            switch self {
+            case .imageData(let imageData): return imageData
+            default: return nil
+            }
+        }
+    }
+}
+
+
